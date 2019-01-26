ea842047ded767adcb540c75cf33a420fd98478a
Jewison Garcia R <30849451+Liknx@users.noreply.github.com>
2019-01-25 23:39:15 -0500

    250120192338

---
+++ src/containers/SignIn.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import '../css/signin.css'
+import banner from '../img/back.jpg'
+import avatar from '../img/avatar.png'
+
+class SignIn extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            usuario: '',
+            contrasena: '',
+            signin: false,
+            showPwd: false
+        }
+    }
+
+    render(){
+        return(
+            <div className="login">
+                {/* <div className="SignIn content-form-signin" style={{backgroundImage: `url(${banner})`}}> */}
+                    <img src={ avatar } alt="profile photo" className="login_avatar"></img>
+                    <form action="index.php" className="login_form">
+                        <div className="login_block">
+                            <label for="username">Username</label>
+                            <input type="text" name="username" id="username" className="login_input" placeholder="Enter your username"></input>
+                        </div>
+                        <div className="login_block">
+                            <label for="username">Password:</label>
+                            <input type="password" name="password" id="password" className="login_input" placeholder="Enter your password"></input>
+                        </div>
+                        <div className="login_block">
+                            <a href="#">Forgot your password?</a>
+                            <br></br>
+                            <a href="#">Reister here!</a>
+                        </div>
+                        <div className="login_block">
+                            <input type="submit" value="LogIn" className="login_btn"></input>
+                        </div>
+                    </form>
+                {/* </div> */}
+            </div>
+        );
+    }
+}
+
+export default SignIn
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SignIn from './containers/SignIn';
+
+ReactDOM.render(
+        <SignIn />
+        ,document.getElementById('root')
+    );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
